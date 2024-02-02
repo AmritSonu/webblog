b89f3369e76043ca5397707a5170ef0071bd7379
AmritSonu <sonus87464@gmail.com>
2024-02-02 22:36:07 +0530

    reployed!

--- backend/index.js
+++ backend/index.js
@@ -14,20 +14,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use((req, res, next) => {
-  const allowedOrigin = "https://web-bee-neon.vercel.app";
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
+app.use(
+  cors({
+    origin: ["https://web-bee-neon.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // ROUTE MOUNTING
